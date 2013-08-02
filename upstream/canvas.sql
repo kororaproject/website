32f43f5c91f4ee28cb8d05b903f4dd2fa5c81c48
firnsy <firnsy@securixlive.com>
2013-08-02 16:48:55 +1000

    added: restructured schema to support multiple package versions and tags for ratings and screenshots.

--- upstream/canvas.sql
+++ upstream/canvas.sql
@@ -6,12 +6,13 @@ DROP TABLE IF EXISTS canvas_accountmembership;
 /* rebuild all tables from scratch */
 DROP TABLE IF EXISTS canvas_user;
 DROP TABLE IF EXISTS canvas_usermembership;
-DROP TABLE IF EXISTS canvas_screenshot;
-DROP TABLE IF EXISTS canvas_rating;
 DROP TABLE IF EXISTS canvas_arch;
+DROP TABLE IF EXISTS canvas_rating;
+DROP TABLE IF EXISTS canvas_screenshot;
+DROP TABLE IF EXISTS canvas_package;
+DROP TABLE IF EXISTS canvas_packagedetails;
 DROP TABLE IF EXISTS canvas_package_ratings;
 DROP TABLE IF EXISTS canvas_package_screenshots;
-DROP TABLE IF EXISTS canvas_package;
 DROP TABLE IF EXISTS canvas_repository;
 DROP TABLE IF EXISTS canvas_comment;
 DROP TABLE IF EXISTS canvas_template_ratings;
@@ -49,7 +50,7 @@ CREATE TABLE canvas_screenshot (
 
 CREATE TABLE canvas_rating (
     id            integer       NOT NULL  PRIMARY KEY  AUTO_INCREMENT,
-    user_id       integer       NOT NULL  REFERENCES canvas_user (id),
+    user_id       integer       NOT NULL  REFERENCES canvas_user(id),
     description   text,
     value         float         NOT NULL
 );
@@ -62,15 +63,17 @@ CREATE TABLE canvas_arch (
 
 CREATE TABLE canvas_package_ratings (
     id            integer       NOT NULL  PRIMARY KEY  AUTO_INCREMENT,
-    package_id    integer       NOT NULL,
-    rating_id     integer       NOT NULL  REFERENCES canvas_rating (id),
+    package_id    integer       NOT NULL  REFERENCES canvas_package(id),
+    rating_id     integer       NOT NULL  REFERENCES canvas_rating(id),
+    tags          text,
     UNIQUE (package_id, rating_id)
 );
 
 CREATE TABLE canvas_package_screenshots (
     id            integer       NOT NULL  PRIMARY KEY  AUTO_INCREMENT,
-    package_id    integer       NOT NULL,
-    screenshot_id integer       NOT NULL  REFERENCES canvas_screenshot (id),
+    package_id    integer       NOT NULL  REFERENCES canvas_package(id),
+    screenshot_id integer       NOT NULL  REFERENCES canvas_screenshot(id),
+    tags          text,
     UNIQUE (package_id, screenshot_id)
 );
 
@@ -81,6 +84,14 @@ CREATE TABLE canvas_package (
     summary       text,
     license       varchar(256),
     url           varchar(256)
+);
+
+CREATE TABLE canvas_packagedetails (
+    id            integer       NOT NULL  PRIMARY KEY  AUTO_INCREMENT,
+    package_id    integer       NOT NULL  REFERENCES canvas_package(id),
+    arch_id       integer       NOT NULL  REFERENCES canvas_arch(id),
+    version       varchar(64),
+    rel           varchar(64)
 );
 
 CREATE TABLE canvas_repository (
@@ -139,8 +150,8 @@ CREATE TABLE canvas_templatepackage (
 
     /* default action is set to INSTALLED only */
     action        integer       NOT NULL DEFAULT 1
-)
-;
+);
+
 CREATE TABLE canvas_templaterepository (
     id            integer       NOT NULL PRIMARY KEY  AUTO_INCREMENT,
     template_id   integer       NOT NULL REFERENCES canvas_template(id),
@@ -150,14 +161,14 @@ CREATE TABLE canvas_templaterepository (
     enabled       bool          NOT NULL DEFAULT TRUE,
     cost          integer       NOT NULL DEFAULT 1000,
     gpg_check     bool          NOT NULL DEFAULT TRUE
-)
-;
+);
+
 CREATE TABLE canvas_machine (
     id            integer       NOT NULL PRIMARY KEY  AUTO_INCREMENT,
     user_id       integer       NOT NULL REFERENCES canvas_user(id),
     template_id   integer       NOT NULL REFERENCES canvas_template(id),
     name          varchar(256)  NOT NULL,
     description   text
-)
-;
+);
+
 COMMIT;
