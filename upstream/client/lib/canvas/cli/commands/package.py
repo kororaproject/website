078b87002db6712bf17fac6b3cf54d09d6b1590c
firnsy <firnsy@kororaproject.org>
2015-09-01 13:44:02 +1000

    added: initial implementation of package add and list.

--- upstream/client/lib/canvas/cli/commands/package.py
+++ upstream/client/lib/canvas/cli/commands/package.py
@@ -16,15 +16,134 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
+import getpass
 import logging
+import prettytable
 
-from .. import commands
+from canvas.cli.commands import Command
+from canvas.package import Package, Repository
+from canvas.service import Service, ServiceException
+from canvas.template import Template
 
 logger = logging.getLogger('canvas')
 
-class PackageCommand(commands.Command):
-  def configure(self, args, args_extra):
-    print("PACKAGE CONFIGURE")
+class PackageCommand(Command):
+  def configure(self, config, args, args_extra):
+    # store loaded config
+    self.config = config
+
+    # create our canvas service object
+    self.cs = Service(host=args.host)
+
+    # store args for additional processing
+    self.args = args
+
+    # return false if any error, help, or usage needs to be shown
+    return not args.help
 
   def run(self):
-    print("PACKAGE RUN")
+    # search for our function based on the specified action
+    command = getattr(self, 'run_{0}'.format(self.args.action))
+
+    if not command:
+      print('error: action is not reachable.')
+      return
+
+    return command()
+
+  def run_add(self):
+    t = Template(self.args.template, user=self.args.username)
+
+    if self.args.username:
+      if not self.cs.authenticate(self.args.username, getpass.getpass('Password ({0}): '.format(self.args.username))):
+        print('error: unable to authenticate with canvas service.')
+        return 1
+
+    try:
+      t = self.cs.template_get(t)
+
+    except ServiceException as e:
+      print(e)
+      return 1
+
+    for p in self.args.package:
+      t.add_package(Package(p))
+
+    packages = list(t.packages_delta)
+    packages.sort(key=lambda x: x.name)
+
+    # describe process for dry runs
+    if self.args.dry_run:
+      if len(packages):
+        print('The following would be added to the template: {0}'.format(t.name))
+
+        for p in packages:
+          print('  - ' + str(p))
+
+        print()
+        print('Summary:')
+        print('  - Package(s): %d' % ( len(packages) ))
+        print()
+
+      else:
+        print('No template changes required.')
+
+      print('No action peformed during this dry-run.')
+      return 0
+
+    if not len(packages):
+      print('info: no changes detected, template up to date.')
+      return 0
+
+    # push our updated template
+    try:
+      res = self.cs.template_update(t)
+
+    except ServiceException as e:
+      print(e)
+      return 1
+
+  def run_list(self):
+    t = Template(self.args.template, user=self.args.username)
+
+    if self.args.username:
+      if not self.cs.authenticate(self.args.username, getpass.getpass('Password ({0}): '.format(self.args.username))):
+        print('error: unable to authenticate with canvas service.')
+        return 1
+
+    try:
+      t = self.cs.template_get(t)
+
+    except ServiceException as e:
+      print(e)
+      return 1
+
+    packages = list(t.packages_all)
+    packages.sort(key=lambda x: x.name)
+
+    repos = list(t.repos_all)
+    repos.sort(key=lambda x: x.name)
+
+    if len(packages):
+      l = prettytable.PrettyTable(["package", "arch"])
+      l.hrules = prettytable.HEADER
+      l.vrules = prettytable.NONE
+      l.align = 'l'
+      l.padding_witdth = 1
+
+      for p in packages:
+        l.add_row([p.name, p.arch])
+
+      print(l)
+      print()
+
+    else:
+      print('0 packages defined.')
+
+  def run_rm(self):
+    print('PACKAGE REMOVE')
+    print(self.args)
+
+  def run_update(self):
+    print('PACKAGE UPDATE')
+    print(self.args)
