20b478f833f264eda0caefca563b7046bf189ce1
firnsy <firnsy@kororaproject.org>
2015-09-01 13:44:14 +1000

    added: initial implementation of repo list.

--- upstream/client/lib/canvas/cli/commands/repo.py
+++ upstream/client/lib/canvas/cli/commands/repo.py
@@ -16,15 +16,83 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
+import getpass
 import logging
+import prettytable
 
-from .. import commands
+from canvas.cli.commands import Command
+from canvas.package import Package, Repository
+from canvas.service import Service, ServiceException
+from canvas.template import Template
 
 logger = logging.getLogger('canvas')
 
-class RepoCommand(commands.Command):
-  def configure(self, args, args_extra):
-    print("REPO CONFIGURE")
+class RepoCommand(Command):
+  def configure(self, config, args, args_extra):
+    # store loaded config
+    self.config = config
+
+    # create our canvas service object
+    self.cs = Service(host=args.host)
+
+    # store args for additional processing
+    self.args = args
+
+    # return false if any error, help, or usage needs to be shown
+    return not args.help
 
   def run(self):
-    print("REPO RUN")
+    # search for our function based on the specified action
+    command = getattr(self, 'run_{0}'.format(self.args.action))
+
+    if not command:
+      print('error: action is not reachable.')
+      return
+
+    return command()
+
+  def run_add(self):
+    print('REPO ADD')
+
+  def run_update(self):
+    print('REPO UPDATE')
+
+  def run_list(self):
+    t = Template(self.args.template, user=self.args.username)
+
+    if self.args.username:
+      if not self.cs.authenticate(self.args.username, getpass.getpass('Password ({0}): '.format(self.args.username))):
+        print('error: unable to authenticate with canvas service.')
+        return 1
+
+    try:
+      t = self.cs.template_get(t)
+
+    except ServiceException as e:
+      print(e)
+      return 1
+
+    packages = list(t.packages_all)
+    packages.sort(key=lambda x: x.name)
+
+    repos = list(t.repos_all)
+    repos.sort(key=lambda x: x.name)
+
+    if len(repos):
+      l = prettytable.PrettyTable(['repo', 'priority', 'cost', 'enabled'])
+      l.hrules = prettytable.HEADER
+      l.vrules = prettytable.NONE
+      l.align = 'l'
+      l.padding_witdth = 1
+
+      for r in repos:
+        l.add_row([r.name, r.priority, r.cost, r.enabled])
+
+      print(l)
+      print()
+
+    else:
+      print('0 repos defined.')
+
+  def run_rm(self):
+    print('REPO REMOVE')
